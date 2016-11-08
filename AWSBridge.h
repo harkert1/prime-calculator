fdcc9e72fb27765851937a808637fd437f1ccb90
Thomas Harker <harkert2@students.rowan.edu>
2016-11-08 16:52:55 -0500

    t

---
+++ AWSBridge.h
@@ -0,0 +1,13 @@
+//
+//  AWSBridge.h
+//  PrimeCalculator
+//
+//  Created by Thomas Harker on 11/6/16.
+//  Copyright © 2016 Thomas Harker. All rights reserved.
+//
+
+#ifndef AWSBridge_h
+#define AWSBridge_h
+
+
+#endif /* AWSBridge_h */
